4ee13277c70a5f7015b931da157b6d91afad85aa
tanmaynag12 <nagtanmay01@gmail.com>
2026-04-20 18:09:11 +0530

    clean server.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,21 @@
 const app = require("../src/server");
 
 module.exports = (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT",
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
+  );
 
-  // Handle preflight
+  // Intercept OPTIONS before Express sees it
   if (req.method === "OPTIONS") {
-    return res.status(200).end();
+    res.status(200).end();
+    return; // ← never call app() for preflight
   }
 
   return app(req, res);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
@@ -8,21 +7,6 @@ const appRoutes = require("./routes/appRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS",
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
 
 app.use(cors());
 
