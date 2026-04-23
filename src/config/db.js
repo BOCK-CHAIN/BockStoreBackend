101b7ab502a8fc758d8bfa00048e5b5dfc41639f
tanmaynag12 <nagtanmay01@gmail.com>
2026-04-23 16:49:39 +0530

    final fix download

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,8 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL ? "FOUND" : "MISSING");
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
